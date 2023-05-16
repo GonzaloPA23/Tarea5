c497df7f08b1442bdf57dce8064581a824a6235b
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 02:23:29 -0500

    Resolución del ejercicio 13

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 /* 
-    EJERCICIO 12
-Ejercicio de filter:
-Dado un arreglo de números, utiliza el método filter para crear un nuevo arreglo que contenga solo los números mayores que 5.
+    EJERCICIO 13
+Ejercicio de reduce:
+Dado un arreglo de números, utiliza el método reduce para calcular el producto de todos los elementos del arreglo.
 */
 
 
-let arregloDeNumeros = [3,324,2,32,32,1,21,2,2,32,4,6];
+let arregloDeNumeros = [3,2,3,2,2,6,2];
 
-let nuevoArregloMayoresA5 = arregloDeNumeros.filter((numero)=>{
-    if(numero>5) 
-    return numero;
+let productoDelArreglo = arregloDeNumeros.reduce((acumulador,numero)=>{
+    return acumulador*=numero;
 })
 
-console.log(nuevoArregloMayoresA5);
+console.log(productoDelArreglo);
