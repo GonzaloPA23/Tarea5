3715ace85e2f17f8d81c961a3c81a7c2a0ffabf7
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 16:10:31 -0500

    Resolución del ejercicio 23

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 /* 
-    EJERCICIO 21
-Ejercicio de reduce:
-Dado un arreglo de palabras, utiliza el método reduce para calcular la cantidad total de caracteres de todas las palabras en el arreglo.
+    EJERCICIO 23
+Ejercicio combinado:
+Dado un arreglo de números, utiliza el método filter para crear un nuevo arreglo que contenga solo los números que sean múltiplos de 3. Luego, utiliza el método reduce para calcular la suma de los números seleccionados.
 */
 
-let arregloPalabras = ["OSO", "BANDA", "PICO", "GASOLINA", "PIÑAS","PROFESOR","codiGO"];
+let arregloDeNumeros = [12,32,35,23,11,3,5,6,9,10,1]
 
-let cantidadTotalDeLetras = arregloPalabras.reduce((acumulador,palabra)=>{
-    return acumulador+=palabra.length;
+let nuevoArregloM3 = arregloDeNumeros.filter((numero)=>{
+    if(numero % 3 === 0){
+        return numero;
+    }
+})
+
+let sumaDelNuevoArreglo = nuevoArregloM3.reduce((acumulador,numero)=>{
+    return acumulador += numero;
 },0)
 
-console.log(cantidadTotalDeLetras);
+console.log("Dado el siguiente arreglo de números:",arregloDeNumeros)
+console.log("El nuevo arreglo de números que son múltiplos de 3: ",nuevoArregloM3);
+console.log("La suma total del nuevo arreglo",sumaDelNuevoArreglo);
