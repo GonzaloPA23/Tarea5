97dd9e19108a63e5a0a1934321ce5682e6c4e717
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 15:18:14 -0500

    Resolución del ejercicio 21

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-/*
-    EJERCICIO 20
-Ejercicio de filter:
-Dado un arreglo de números, utiliza el método filter para crear un nuevo arreglo que contenga solo los números impares.
+/* 
+    EJERCICIO 21
+Ejercicio de reduce:
+Dado un arreglo de palabras, utiliza el método reduce para calcular la cantidad total de caracteres de todas las palabras en el arreglo.
 */
 
-let arregloNumeros=[13,32,2,1,32,43,2,1,63,80]
+let arregloPalabras = ["OSO", "BANDA", "PICO", "GASOLINA", "PIÑAS","PROFESOR","codiGO"];
 
-let nuevoArregloDeImpares = arregloNumeros.filter((numero)=>{
-    if(numero%2!==0){
-        return numero;
-    }
-})
+let cantidadTotalDeLetras = arregloPalabras.reduce((acumulador,palabra)=>{
+    return acumulador+=palabra.length;
+},0)
 
-console.log(nuevoArregloDeImpares)
+console.log(cantidadTotalDeLetras);
