68c5286a90fdf52e945c49895a787a9fc21f1f15
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 17:45:44 -0500

    Resolución del ejercicio 26

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 /* 
-    EJERCICIO 25
-Escribe un bucle que imprima la serie de Fibonacci hasta el décimo término. (La serie de Fibonacci comienza con 0 y 1, y cada término siguiente es la suma de los dos anteriores).
+        EJERCICIO 26
+    Ejercicio de condicionales:
+Escribe una función que tome una cadena como argumento y devuelva "Es un palíndromo" si la cadena es igual al revés, y "No es un palíndromo" si no lo es.
 */
 
-let Fibonacci = (numero) =>{
-    let fb = [0,1];
-    for(let i = 2; i<=numero; i++){
-        fb[i] = fb[i-1] + fb[i-2];
+/* Forma 1 */
+
+let palindromo = (palabra) => {
+  palabra = palabra.toLowerCase();
+  for (let i = 0; i < palabra.length; i++) {
+    if (palabra[i] !== palabra[palabra.length - i - 1]) {
+      return "No es palindromo";
     }
-    return fb;
-}
-console.log(Fibonacci(10));
+  }
+  return "Es palindromo";
+};
+
+console.log(palindromo("Amor a Roma"));
+console.log(palindromo("Radar"));
+console.log(palindromo("Arbol"));
+console.log(palindromo("ALÁ"));
+console.log(palindromo("ALA"));
+
+/* Forma 2 */
+console.log("\n MANERA 2 :")
+const esPalindromo = (text) => {
+    text = text.toLowerCase(); // Convertir a minúsculas y eliminar espacios en blanco
+    return text === text.split('').reverse().join('');
+  };
+  
+  console.log(esPalindromo("Amor a Roma")); // true
+  console.log(esPalindromo("RADAR")); // true
+  console.log(esPalindromo("Ala")); // true
+  console.log(esPalindromo("Esternocleidomastoideo")); // false
