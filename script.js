0f71aff2efc87f3b3f0ab7a9ceffe3dfbe678af5
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 01:47:59 -0500

    Resolución del ejercicio 10

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 /* 
-    EJERCICIO 9
-Ejercicio de bucles:
-Escribe un bucle que imprima los números del 10 al 1 en la consola, en orden descendente.
+    EJERCICIO 10
+Ejercicio de condicionales:
+Escribe una función que tome un número como argumento y devuelva "Positivo" si el número es mayor que 0, "Negativo" si el número es menor que 0, y "Cero" si el número es igual a 0.
 */
 
-for (let i = 10; i > 0; i--) {
-  console.log(i);
+let EsPositivoNegativo = (numero) =>{
+    if(numero > 0){
+        console.log(`El número ${numero} es positivo`)
+    }else if(numero === 0){
+        console.log(`El número ${numero} es cero`)
+    }else{console.log(`El número ${numero} es negativo`)}
 }
+
+EsPositivoNegativo(-2343);
