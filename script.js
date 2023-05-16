4dd418e8064a5f8507e2c4340d5e1716c269e71a
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 02:20:51 -0500

    Resolución del ejercicio 12

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 /* 
-    EJERCICIO 11
-Ejercicio de map:
-Dado un arreglo de palabras, utiliza el método map para crear un nuevo arreglo que contenga la longitud de cada palabra.
+    EJERCICIO 12
+Ejercicio de filter:
+Dado un arreglo de números, utiliza el método filter para crear un nuevo arreglo que contenga solo los números mayores que 5.
 */
 
-let arregloDePalabras = ["legal","hablaremos","cambió","solas","prisionero","odie","ocupada","llevarlo","estuvimos","decírselo"]
 
-let nuevoArregloDeLongitudes = arregloDePalabras.map((texto)=>{
-    return texto.length;
+let arregloDeNumeros = [3,324,2,32,32,1,21,2,2,32,4,6];
+
+let nuevoArregloMayoresA5 = arregloDeNumeros.filter((numero)=>{
+    if(numero>5) 
+    return numero;
 })
 
-console.log("Este es el arreglo de palabras: ",arregloDePalabras)
-console.log("Este es el arreglo de longitudes de las palabras dadas: ",nuevoArregloDeLongitudes);
+console.log(nuevoArregloMayoresA5);
