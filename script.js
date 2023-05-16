c9498f631071b75693069c4dffacdf10036362ab
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-15 23:48:54 -0500

    Resolución del ejercicio 5

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /* 
-    EJERCICIO 4
-Ejercicio de filter:
-Dado un arreglo de números, utiliza el método filter para crear un nuevo arreglo que contenga solo los números pares.
+    EJERCICIO 5
+Ejercicio de reduce:
+Dado un arreglo de números, utiliza el método reduce para calcular la suma de todos los elementos del arreglo.
 */
 
 let arregloNumeros = [2,5,10,30,27,1,40,-20]
-let numerosPares = arregloNumeros.filter((numero)=>{
-    if(numero%2 === 0){
-        return true;
-    }
-})
+
+let sumaNumeros = arregloNumeros.reduce((acumulador,numero)=>{
+    return acumulador += numero;
+},0)
 
 console.log("Dado el siguiente arreglo: ", arregloNumeros);
-console.log("El núevo arreglo con solo los números pares es:", numerosPares);
+console.log("La suma total del arreglo es: ", sumaNumeros);
+
