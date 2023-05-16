3e14aea891c5020ce7f26b1f62f8fb3654ed4e3d
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 02:59:43 -0500

    Resolución del ejercicio 15

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 /* 
-    EJERCICIO 13
-Ejercicio de reduce:
-Dado un arreglo de números, utiliza el método reduce para calcular el producto de todos los elementos del arreglo.
+    EJERCICIO 15
+Ejercicio combinado:
+Dado un arreglo de palabras, utiliza el método filter para crear un nuevo arreglo que contenga solo las palabras que contienen la letra "a". Luego, utiliza el método map para convertir cada palabra en minúsculas.
 */
 
+let arregloPalabras = [
+  "OSO",
+  "BANDA",
+  "PICO",
+  "GASOLINA",
+  "PIÑAS",
+  "PROFESOR",
+  "codiGO",
+];
 
-let arregloDeNumeros = [3,2,3,2,2,6,2];
+/* Forma 1 */
 
-let productoDelArreglo = arregloDeNumeros.reduce((acumulador,numero)=>{
-    return acumulador*=numero;
-})
+let arregloDePalabrasConA = arregloPalabras.filter((palabra) => {
+  for (let i = 0; i < palabra.length; i++) {
+    if (palabra[i] === "A" || palabra[i] === "a") {
+      return true;
+    }
+  }
+});
 
-console.log(productoDelArreglo);
+let arregloEnMinusculas = arregloDePalabrasConA.map((palabra) => {
+  return palabra.toLowerCase();
+});
+
+console.log("Forma 1: ", arregloEnMinusculas);
+
+/* Forma 2 */
+
+let arrEnMinuscula = arregloPalabras
+  .filter((palabra) => palabra.toLowerCase().includes("a"))
+  .map((palabra) => palabra.toLowerCase());
+
+console.log("Forma 2: ");
+console.log(arrEnMinuscula);
