4710e9fd235775e30d0c65ac36d4fa115d36cd47
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 14:34:12 -0500

    Resolución del ejercicio 18

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 /* 
-    EJERCICIO 17
-Ejercicio de bucles:
-Escribe un bucle que imprima los números pares del 1 al 20 en la consola.
+    EJERCICIO 18
+Ejercicio de condicionales:
+Escribe una función que tome una cadena como argumento y devuelva "Es una frase" si la cadena contiene espacios en blanco, y "Es una palabra" si la cadena no contiene espacios en blanco.
 */
 
-for(let i = 2; i < 21;i+=2){
-    console.log(i);
-}
+let validarFraseOPalabra = (cadena) => {
+  let contieneEspacio = false;
+  for (i = 0; i < cadena.length; i++) {
+    if (cadena[i] === " ") {
+      contieneEspacio = true;
+    }
+  }
+
+  if (contieneEspacio) {
+    console.log("Es una frase:", cadena);
+  } else {
+    console.log("Es una palabra:", cadena);
+  }
+};
+
+validarFraseOPalabra("Mañana no hay clases");
