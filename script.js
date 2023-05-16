ad8c452e580fc837603b02310e2ad940eeeea178
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 03:03:44 -0500

    Resolución del ejercicio 17

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
 /* 
-    EJERCICIO 15
-Ejercicio combinado:
-Dado un arreglo de palabras, utiliza el método filter para crear un nuevo arreglo que contenga solo las palabras que contienen la letra "a". Luego, utiliza el método map para convertir cada palabra en minúsculas.
+    EJERCICIO 17
+Ejercicio de bucles:
+Escribe un bucle que imprima los números pares del 1 al 20 en la consola.
 */
 
-let arregloPalabras = [
-  "OSO",
-  "BANDA",
-  "PICO",
-  "GASOLINA",
-  "PIÑAS",
-  "PROFESOR",
-  "codiGO",
-];
-
-/* Forma 1 */
-
-let arregloDePalabrasConA = arregloPalabras.filter((palabra) => {
-  for (let i = 0; i < palabra.length; i++) {
-    if (palabra[i] === "A" || palabra[i] === "a") {
-      return true;
-    }
-  }
-});
-
-let arregloEnMinusculas = arregloDePalabrasConA.map((palabra) => {
-  return palabra.toLowerCase();
-});
-
-console.log("Forma 1: ", arregloEnMinusculas);
-
-/* Forma 2 */
-
-let arrEnMinuscula = arregloPalabras
-  .filter((palabra) => palabra.toLowerCase().includes("a"))
-  .map((palabra) => palabra.toLowerCase());
-
-console.log("Forma 2: ");
-console.log(arrEnMinuscula);
+for(let i = 2; i < 21;i+=2){
+    console.log(i);
+}
