d75a324a97915a54b6214ab27cee39c99ed432a8
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-15 23:44:58 -0500

    Resolución del ejercicio 4

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 /* 
-    EJERCICIO 3
-Ejercicio de map:
-Dado un arreglo de números, utiliza el método map para crear un nuevo arreglo que contenga el cuadrado de cada número(sin usar Math.pow()).
+    EJERCICIO 4
+Ejercicio de filter:
+Dado un arreglo de números, utiliza el método filter para crear un nuevo arreglo que contenga solo los números pares.
 */
 
-let arregloNumeros = [2,5,10,20,30,40]
-
-let arregloAlCuadrado = arregloNumeros.map((_elemento)=>{
-    return _elemento * _elemento;
+let arregloNumeros = [2,5,10,30,27,1,40,-20]
+let numerosPares = arregloNumeros.filter((numero)=>{
+    if(numero%2 === 0){
+        return true;
+    }
 })
 
-console.log("Dado el siguiente arreglo",arregloNumeros,"el cuadrado de cada número será: ")
-arregloAlCuadrado.forEach((numero,index) =>{
-    console.log(arregloNumeros[index],"^ 2 = ",numero);
-})
-
-console.log("El núevo arreglo al cuadrado es: ",arregloAlCuadrado);
+console.log("Dado el siguiente arreglo: ", arregloNumeros);
+console.log("El núevo arreglo con solo los números pares es:", numerosPares);
