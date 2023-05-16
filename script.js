ba19381b83e5f803bc9c6a33dd84282c450d51ad
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 01:43:24 -0500

    Resolución del ejercicio 9

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 /* 
-    EJERCICIO 6
-Ejercicio combinado:
-Dado un arreglo de palabras, utiliza un bucle y condicionales para crear un nuevo arreglo que contenga solo las palabras que tienen más de 5 caracteres. Luego, utiliza el método map para convertir cada palabra en mayúsculas.
+    EJERCICIO 9
+Ejercicio de bucles:
+Escribe un bucle que imprima los números del 10 al 1 en la consola, en orden descendente.
 */
 
-let arregloPalabras = ["peruano", "banda", "manzana", "pera", "gasolina", "piñas","profesor","codiGO"];
-
-let LaPalabraEsMayorA5 = () => {
-  let palabrasMayoresA5 = [];
-  for (let i = 0; i < arregloPalabras.length; i++) {
-    if (arregloPalabras[i].length > 5) {
-      palabrasMayoresA5.push(arregloPalabras[i]);
-    }
-  }
-  return palabrasMayoresA5;
+for (let i = 10; i > 0; i--) {
+  console.log(i);
 }
-
-let arregloEnMayusculas = LaPalabraEsMayorA5().map((texto)=>{
-    return texto.toUpperCase();
-})
-
-console.log("Dado el arreglo: ", arregloPalabras)
-console.log("El nuevo arreglo de palabras en MAYÚSCULAS con letras > 5 es: ",arregloEnMayusculas);
