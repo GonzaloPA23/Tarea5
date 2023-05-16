b9431d3f47f5216a7987ecea927ddec115d55d9b
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 17:56:16 -0500

    Resolución del ejercicio 27

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
 /* 
-        EJERCICIO 26
-    Ejercicio de condicionales:
-Escribe una función que tome una cadena como argumento y devuelva "Es un palíndromo" si la cadena es igual al revés, y "No es un palíndromo" si no lo es.
+        EJERCICIO 27
+    Ejercicio de map:
+Dado un arreglo de números, utiliza el método map para crear un nuevo arreglo que contenga el doble de cada número.
 */
 
-/* Forma 1 */
+let arregloDeNumeros = [10,20,30,40]
 
-let palindromo = (palabra) => {
-  palabra = palabra.toLowerCase();
-  for (let i = 0; i < palabra.length; i++) {
-    if (palabra[i] !== palabra[palabra.length - i - 1]) {
-      return "No es palindromo";
-    }
-  }
-  return "Es palindromo";
-};
+let dobleDelArreglo = arregloDeNumeros.map((_elemento)=>{ 
+    return _elemento * 2;
+})
 
-console.log(palindromo("Amor a Roma"));
-console.log(palindromo("Radar"));
-console.log(palindromo("Arbol"));
-console.log(palindromo("ALÁ"));
-console.log(palindromo("ALA"));
-
-/* Forma 2 */
-console.log("\n MANERA 2 :")
-const esPalindromo = (text) => {
-    text = text.toLowerCase(); // Convertir a minúsculas y eliminar espacios en blanco
-    return text === text.split('').reverse().join('');
-  };
-  
-  console.log(esPalindromo("Amor a Roma")); // true
-  console.log(esPalindromo("RADAR")); // true
-  console.log(esPalindromo("Ala")); // true
-  console.log(esPalindromo("Esternocleidomastoideo")); // false
+console.log(dobleDelArreglo)
