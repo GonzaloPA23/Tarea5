d465acfac73f509715fe337e535c2f0a66c7b8da
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 21:42:49 -0500

    Resolución del ejercicio 36

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /* 
-        EJERCICIO 35
-Ejercicio de map:
-Dado un arreglo de palabras, utiliza el método map para crear un nuevo arreglo que contenga la primera letra de cada palabra.
+        EJERCICIO 36
+Dado un arreglo de números, utiliza el método filter para crear un nuevo arreglo que contenga solo los números divisibles por 3.
 */
 
-let arregloDePalabras = ["perro","gato","pizarra","marisco","pizza","comida","chef","ganador","vilchez"];
+let arregloDeNumeros = [30,3,1,23,45,67,10,2,5]
 
-let nuevoArregloPrimeraLetra = arregloDePalabras.map((palabra)=>{
-    return palabra[0];
+let nuevoArregloDivisiblePor3 = arregloDeNumeros.filter((num)=>{
+    if(num % 3 === 0){
+        return num;
+    }
 })
 
-console.log(arregloDePalabras);
-console.log(nuevoArregloPrimeraLetra);
+console.log(nuevoArregloDivisiblePor3);
