3882f9ad1ba95bc0568bd74992b9d9ea30adf1e6
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 23:46:53 -0500

    Resolución del ejercicio 46

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
 /* 
-        EJERCICIO 44
-Ejercicio de funciones:
-Crea una función llamada calcularPromedio que tome un arreglo de números como argumento y devuelva el promedio de esos números.
+        EJERCICIO 46
+Ejercicio de funciones anónimas:
+Crea una función anónima que tome un arreglo de palabras como argumento y devuelva un nuevo arreglo con la longitud de cada palabra.
 */
 
-function calcularPromedio(...numeros) {
-  let p = 0;
-  let suma = 0;
-  for (let numero of numeros) {
-    suma += numero;
-  }
-  return (p = suma / numeros.length);
-}
-console.log("Del ejercicio 44:");
-console.log(calcularPromedio(1, 2, 3));
-console.log(calcularPromedio(18.5, 12.6, 15.6, 17.9));
-
-/* 
-        EJERCICIO 45
-Ejercicio de funciones flecha:
-Convierte la función calcularPromedio en una función flecha.
-*/
-let calcularPromedio2 = (...numbers) => {
-  let promedio = 0;
-  let sumita = 0;
-  for (let num of numbers) {
-    sumita += num;
-  }
-  return (promedio = sumita / numbers.length);
+const obtenerLongitudPalabras = (...arreglo) => {
+  return arreglo.map((palabra) => palabra.length);
 };
-console.log("Del ejercicio 45:");
-console.log(calcularPromedio2(43, 43, 32, 34, 23));
-console.log(calcularPromedio2(20, 12.5, 20, 12.5));
+
+const palabras = ["Hola", "mundo", "JavaScript", "funciones", "anónimas","Aprendiendo mucho"];
+const longitudes = obtenerLongitudPalabras(...palabras);
+
+console.log(palabras);
+console.log(longitudes); // [4, 5, 10, 9, 8]
