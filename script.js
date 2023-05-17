11945e6cf414507398df0eb47b24f2797dd26cbf
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-16 23:59:19 -0500

    Resolución del ejercicio 62

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 /* 
-        EJERCICIO 61
-Ejercicio de funciones:
-Crea una función llamada calcularFactorial que tome un número como argumento y devuelva el factorial de ese número.
+        EJERCICIO 62
+Ejercicio de funciones flecha:
+Convierte la función calcularFactorial en una función flecha.
 */
 
-function calcularFactorial(n) {
+let calcularFactorial = (n) => {
   if (n === 0 || n === 1) {
     return 1;
   } else {
     return n * calcularFactorial(n - 1);
   }
-}
+};
 
-let numero = 5;
-console.log(`${numero}! =`,calcularFactorial(numero)); // 120
+let numero = 6;
+console.log(`${numero}! =`, calcularFactorial(numero)); // 120
