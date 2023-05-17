28be3e4163ac939d63f8786b46441400efd96699
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-05-17 01:26:48 -0500

    Resolución del ejercicio 75

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /* 
-        EJERCICIO 74
-Ejercicio de funciones:
-Crea una función llamada filtrarPositivos que tome un arreglo de números como argumento y devuelva un nuevo arreglo con solo los números positivos.
+        EJERCICIO 75
+Ejercicio de funciones flecha:
+Convierte la función filtrarPositivos en una función flecha.
 */
 
-function filtrarPositivos(numeros) {
-  return numeros.filter((numero) => numero > 0);
-}
+
+const filtrarPositivos = (numeros) => {
+  return numeros.filter(numero => numero > 0);
+};
 
 // Ejemplo de uso
 const numeros = [-2, 5, -10, 8, -3, 0, 12];
 console.log(filtrarPositivos(numeros)); // [5, 8, 12]
-
